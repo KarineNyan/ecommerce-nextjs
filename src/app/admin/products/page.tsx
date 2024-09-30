095a6f6ba1922b84c63dcd8a33ab71c6b842b0df
KarineNyan <br.karine.fernanda@gmail.com>
2024-09-30 20:59:32 -0300

    creating products admin page

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,3 +1,36 @@
+import { Button } from "@/components/ui/button"
+
+import Link from "next/link"
+import { Table, TableHeader, TableRow, TableHead, TableBody } from "@/components/ui/table"
+import { PageHeader } from "../_components/PageHeader"
+
 export default function AdminProductsPage() {
-    return <h1>Hi</h1>
+    return (
+        <>
+            <div className="flex justify-between items-center gap-4">
+                <PageHeader>Products</PageHeader>
+                <Button asChild>
+                    <Link href="/admin/products/new">Add Product</Link>
+                </Button>
+            </div>
+            <ProductsTable />
+        </>
+    )
+}
+
+function ProductsTable() {
+    return <Table>
+        <TableHeader>
+            <TableRow>
+                <TableHead className="w-0"> <span className="sr-only">Available For Purchase</span>
+                </TableHead>
+                <TableHead>Name</TableHead>
+                <TableHead>Price</TableHead>
+                <TableHead>Orders</TableHead>
+                <TableHead className="w-0"> <span className="sr-only">Actions</span>
+                </TableHead>
+            </TableRow>
+        </TableHeader>
+        <TableBody></TableBody>
+    </Table>
 }
